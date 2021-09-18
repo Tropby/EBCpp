3fa67ae1ce3a853eb0202f2bf78b3404e10e45a2
Tropby <Tropby@gmx.net>
2021-09-18 18:21:28 +0200

    Added an signal slot example.

---
+++ examples/ExampleSignalSlot.cpp
@@ -0,0 +1,51 @@
+
+#include "../src/EBObject.hpp"
+#include "../src/EBEvent.hpp"
+
+using namespace EBCpp;
+
+class ExampleSignal : public EBObject<ExampleSignal>
+{
+public:
+    ExampleSignal()
+    {
+    }
+
+    EB_SIGNAL(helloWorldCalled);
+
+    void helloWorld()
+    {
+        EB_EMIT(helloWorldCalled);
+    }
+
+private:
+    
+};
+
+class ExampleSlot : public EBObject<ExampleSlot>
+{
+public:
+    ExampleSlot()
+    {
+    }
+
+    EB_SLOT(helloWorld)
+    {
+        std::cout << "Hello World!" << std::endl;
+                
+        EBEventLoop::getInstance()->exit();
+    }
+
+private:
+};
+
+int main()
+{
+    ExampleSignal signal;
+    ExampleSlot slot;
+
+    signal.helloWorldCalled.connect((&slot).get(), &ExampleSlot::helloWorld);
+    signal.helloWorld();
+
+    EBEventLoop::getInstance()->exec();
+}
